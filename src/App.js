2cec3e78a95b1177dd68b96183b2ad6dcc01b0d4
LunaFreya Dragonborn <rose.sky000801@gmail.com>
2024-09-06 18:27:44 +0200

    Set up test alert for axios and loading spinner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import Weather from './Weather';
 import './App.css';
 
 function App() {
@@ -6,17 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App</h1>
+        <Weather city="Tokyo"/>
       </header>
     </div>
   );
